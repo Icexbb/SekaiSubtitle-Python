4410280e087cee19731fc208fa2841f3ac6cb288
icexb <icexb_baka@163.com>
2023-03-23 20:44:58 +0800

    Adjust Banner Fade Effect and Time

--- src/gui/gui_main.py
+++ src/gui/gui_main.py
@@ -23,7 +23,7 @@ from gui.widgets.widget_translate import TranslateWidget
 EXIT_CODE_REBOOT = -11231351
 
 PROGRAM_NAME = "Sekai Subtitle"
-VERSION = "v0.7.2"
+VERSION = "v0.7.3"
 
 
 class MainUi(FramelessMainWindow, Ui_MainWindow):
